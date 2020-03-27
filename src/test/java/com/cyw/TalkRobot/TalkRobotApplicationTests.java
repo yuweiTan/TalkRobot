d4d970f80973a305809833d03d20f3ef4e052c7e
微风 <528461356@qq.com>
2020-03-27 20:25:36 +0800

    测试

--- src/test/java/com/cyw/TalkRobot/TalkRobotApplicationTests.java
+++ src/test/java/com/cyw/TalkRobot/TalkRobotApplicationTests.java
@@ -1,8 +1,12 @@
 package com.cyw.TalkRobot;
 
+import com.alibaba.fastjson.JSONObject;
+import com.cyw.TalkRobot.Enums.ActionEnum;
+import com.cyw.TalkRobot.entity.PO.response.ActionListPO;
+import com.cyw.TalkRobot.entity.PO.response.ResponseListPO;
+import com.cyw.TalkRobot.entity.PO.response.ResponsePO;
 import com.cyw.TalkRobot.entity.PO.skill.*;
 import com.cyw.TalkRobot.util.HttpUtil;
-import net.minidev.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -82,8 +86,8 @@ public class TalkRobotApplicationTests {
 		try {
 			// 请求参数
 			Robot robot = new Robot();
-			com.entity.PO.robot.RequestPO requestPO = new com.entity.PO.robot.RequestPO(user_id, query);
-			com.entity.PO.robot.ParamsPO paramsPO = new com.entity.PO.robot.ParamsPO(version, service_id, log_id, session_id, requestPO);
+			com.cyw.TalkRobot.entity.PO.robot.RequestPO requestPO = new com.cyw.TalkRobot.entity.PO.robot.RequestPO(user_id, query);
+			com.cyw.TalkRobot.entity.PO.robot.ParamsPO paramsPO = new com.cyw.TalkRobot.entity.PO.robot.ParamsPO(version, service_id, log_id, session_id, requestPO);
 //            String params = "{\"log_id\":\"UNITTEST_10000\",\"version\":\"2.0\",\"session_id\":\"\",\"skill_ids\":[\"60516\",\"60517\",\"60518\",\"60519\"],\"request\":{\"query\":\"杭州天气\",\"user_id\":\"88888\"}}";
 			String params = JSONObject.toJSONString(paramsPO);
 			String result = HttpUtil.post(talkUrl, accessToken, "application/json", params);
@@ -99,98 +103,96 @@ public class TalkRobotApplicationTests {
 	public void contextLoads() {
 
 
-
-		public static void main(String[] args) {
 //        String auth = getAuth();
 //        System.out.println(auth);
-			while (true) {
-				Scanner scanner = new Scanner(System.in);
-				String next = scanner.next();
-				query = next;
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			String next = scanner.next();
+			query = next;
 //            String utterance = utterance();
-				String utterance = robot();
-				ResponsePO responsePO = JSONObject.parseObject(utterance, ResponsePO.class);
-				java.util.List<ResponseListPO> response_list = responsePO.getResult().getResponse_list();
-				for (ResponseListPO responseListPO : response_list) {
-					java.util.List<ActionListPO> action_list = responseListPO.getAction_list();
-					for (ActionListPO actionListPO : action_list) {
-						if (!actionListPO.getType().equals(ActionEnum.failure.getKey()))
-							System.out.println(actionListPO.getSay());
-					}
+			String utterance = robot();
+			ResponsePO responsePO = JSONObject.parseObject(utterance, ResponsePO.class);
+			java.util.List<ResponseListPO> response_list = responsePO.getResult().getResponse_list();
+			for (ResponseListPO responseListPO : response_list) {
+				java.util.List<ActionListPO> action_list = responseListPO.getAction_list();
+				for (ActionListPO actionListPO : action_list) {
+					if (!actionListPO.getType().equals(ActionEnum.failure.getKey()))
+						System.out.println(actionListPO.getSay());
 				}
-				System.out.println(utterance);
-				if ("再见".equals(next))
-					return;
 			}
+			System.out.println(utterance);
+			if ("再见".equals(next))
+				return;
 		}
-
-		/**
-		 * 获取权限token
-		 * @return 返回示例：
-		 * {
-		 * "access_token": "24.460da4889caad24cccdb1fea17221975.2592000.1491995545.282335-1234567",
-		 * "expires_in": 2592000
-		 * }
-		 */
-		public static String getAuth() {
-			// 官网获取的 API Key 更新为你注册的
-			String clientId = "G3pyH3oAKMvGlUrZdtYItzVs";
-			// 官网获取的 Secret Key 更新为你注册的
-			String clientSecret = "orjSxe4AdPoOTP40zmgKUkDDtH34cBSu";
-			return getAuth(clientId, clientSecret);
-		}
-
-		/**
-		 * 获取API访问token
-		 * 该token有一定的有效期，需要自行管理，当失效时需重新获取.
-		 * @param ak - 百度云官网获取的 API Key
-		 * @param sk - 百度云官网获取的 Securet Key
-		 * @return assess_token 示例：
-		 * "24.460da4889caad24cccdb1fea17221975.2592000.1491995545.282335-1234567"
-		 */
-		public static String getAuth(String ak, String sk) {
-			// 获取token地址
-			String authHost = "https://aip.baidubce.com/oauth/2.0/token?";
-			String getAccessTokenUrl = authHost
-					// 1. grant_type为固定参数
-					+ "grant_type=client_credentials"
-					// 2. 官网获取的 API Key
-					+ "&client_id=" + ak
-					// 3. 官网获取的 Secret Key
-					+ "&client_secret=" + sk;
-			try {
-				URL realUrl = new URL(getAccessTokenUrl);
-				// 打开和URL之间的连接
-				HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
-				connection.setRequestMethod("GET");
-				connection.connect();
-				// 获取所有响应头字段
-				Map<String, List<String>> map = connection.getHeaderFields();
-				// 遍历所有的响应头字段
-				for (String key : map.keySet()) {
-					System.err.println(key + "--->" + map.get(key));
-				}
-				// 定义 BufferedReader输入流来读取URL的响应
-				BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-				String result = "";
-				String line;
-				while ((line = in.readLine()) != null) {
-					result += line;
-				}
-				/**
-				 * 返回结果示例
-				 */
-				System.err.println("result:" + result);
-				JSONObject jsonObject = new JSONObject(Boolean.parseBoolean(result));
-				String access_token = jsonObject.getString("access_token");
-				return access_token;
-			} catch (Exception e) {
-				System.err.printf("获取token失败！");
-				e.printStackTrace(System.err);
-			}
-			return null;
-		}
-
 	}
+
+	/**
+	 * 获取权限token
+	 * @return 返回示例：
+	 * {
+	 * "access_token": "24.460da4889caad24cccdb1fea17221975.2592000.1491995545.282335-1234567",
+	 * "expires_in": 2592000
+	 * }
+	 */
+	public static String getAuth() {
+		// 官网获取的 API Key 更新为你注册的
+		String clientId = "G3pyH3oAKMvGlUrZdtYItzVs";
+		// 官网获取的 Secret Key 更新为你注册的
+		String clientSecret = "orjSxe4AdPoOTP40zmgKUkDDtH34cBSu";
+		return getAuth(clientId, clientSecret);
+	}
+
+	/**
+	 * 获取API访问token
+	 * 该token有一定的有效期，需要自行管理，当失效时需重新获取.
+	 * @param ak - 百度云官网获取的 API Key
+	 * @param sk - 百度云官网获取的 Securet Key
+	 * @return assess_token 示例：
+	 * "24.460da4889caad24cccdb1fea17221975.2592000.1491995545.282335-1234567"
+	 */
+	public static String getAuth(String ak, String sk) {
+		// 获取token地址
+		String authHost = "https://aip.baidubce.com/oauth/2.0/token?";
+		String getAccessTokenUrl = authHost
+				// 1. grant_type为固定参数
+				+ "grant_type=client_credentials"
+				// 2. 官网获取的 API Key
+				+ "&client_id=" + ak
+				// 3. 官网获取的 Secret Key
+				+ "&client_secret=" + sk;
+		try {
+			URL realUrl = new URL(getAccessTokenUrl);
+			// 打开和URL之间的连接
+			HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
+			connection.setRequestMethod("GET");
+			connection.connect();
+			// 获取所有响应头字段
+			Map<String, List<String>> map = connection.getHeaderFields();
+			// 遍历所有的响应头字段
+			for (String key : map.keySet()) {
+				System.err.println(key + "--->" + map.get(key));
+			}
+			// 定义 BufferedReader输入流来读取URL的响应
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String result = "";
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+			/**
+			 * 返回结果示例
+			 */
+			System.err.println("result:" + result);
+			JSONObject jsonObject = new JSONObject(Boolean.parseBoolean(result));
+			String access_token = jsonObject.getString("access_token");
+			return access_token;
+		} catch (Exception e) {
+			System.err.printf("获取token失败！");
+			e.printStackTrace(System.err);
+		}
+		return null;
+	}
+
+
 
 }
