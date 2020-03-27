8c0a111b24cd7c418e4e006ede9e83f877671866
微风 <528461356@qq.com>
2020-03-27 15:47:53 +0800

    框架

---
+++ src/main/java/com/cyw/TalkRobot/TalkRobotApplication.java
@@ -0,0 +1,13 @@
+package com.cyw.TalkRobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkRobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyw/TalkRobot/TalkRobotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cyw.TalkRobot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TalkRobotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
